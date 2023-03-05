c5f744206be60de6cadc2c1eb43ea85851dc856f
juliidiaz <105684155+juliidiaz@users.noreply.github.com>
2023-03-05 09:03:24 -0300

    Add files via upload

---
+++ JS/javascript.js
@@ -0,0 +1,7 @@
+let ingreso = prompt ("Sos mayor de edad? Contestar: si/no");
+
+if (ingreso == "si") {
+    alert ("Bienvenid@")
+}
+else {
+    alert ("Ud. está entrando bajo su responsabilidad");}
